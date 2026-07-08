e7b4f0c1c866e9a372e1944fd9fa40a5adb53880
cohenor28-cmd <cohenor28@gmail.com>
2026-07-08 09:50:09 +0000

    v44: mobile system check

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='drawlab-v43-mobile-lab';
+const CACHE_NAME='drawlab-v44-mobile-lab';
 const ASSETS=['./','./index.html','./manifest.json','./data/I1_0910.csv','./data/I1_1011.csv','./data/I1_1112.csv','./data/I1_1213.csv','./data/I1_1314.csv'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
